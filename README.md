f613d041c8be566f8198387f14f0912cc9392897
Nirajan Mahara <np03a190125@heraldcollege.edu.np>
2021-04-06 16:51:43 +0545

    Fix responsives to allow cross-device to use navigation and web-app

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # apple-webiste-clone
+
+git clone https
+cd apple-website-clone
+git status
+git add .
+git status
+git commit -m "verb mesage"
+git remote -v
+git push / git push -u origin main
--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-//JavaScript
+const selectElement = (element) => document.querySelector(element);
+
+// toggle mobile menu
+selectElement(".mobile-menu").addEventListener("click", () => {
+  selectElement("header").classList.toggle("active");
+});
